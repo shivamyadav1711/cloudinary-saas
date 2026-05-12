a0c9161cd072336e3fcf39762ae139153805863e
Kumar Shivam Prakash <krshivamvj@gmail.com>
2026-05-13 01:01:30 +0530

    checked

--- middleware.ts
+++ middleware.ts
@@ -3,36 +3,15 @@ import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
-  "/home",
   "/sign-in(.*)",
   "/sign-up(.*)",
-]);
-
-const isPublicApiRoute = createRouteMatcher([
-  "/api/videos",
 ]);
 
 export default clerkMiddleware((auth, req) => {
   const { userId } = auth();
 
-  const currentUrl = new URL(req.url);
-  const isAccessingDashboard = currentUrl.pathname === "/home";
-  const isApiRequest = currentUrl.pathname.startsWith("/api");
-
-  // Logged in users
-  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
-    return NextResponse.redirect(new URL("/home", req.url));
-  }
-
-  // Not logged in users
-  if (!userId) {
-    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/sign-in", req.url));
-    }
-
-    if (isApiRequest && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/sign-in", req.url));
-    }
+  if (!userId && !isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
   return NextResponse.next();
